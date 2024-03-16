693d3b4fce5248c728d2b83bf5fed2a4c72fd837
DAVID CERNY <david.cerny.021@pslib.cz>
2024-03-16 18:34:43 +0100

    added bullet effects range and buncha silly stuff

--- Assets/Scripts/AttractiveBulletBehaviour.cs
+++ Assets/Scripts/AttractiveBulletBehaviour.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class AttractiveBulletBehaviour : BulletBehaviour
 {
     public float attractionForce = 9.81f;
+    public float attractionRange = 10f;
 
     protected override void ApplyForce(GameObject obj)
     {
-        if (obj != centerOfGravity)
+        if (obj != centerOfGravity && Vector3.Distance(centerOfGravity.transform.position, obj.transform.position) <= attractionRange)
         {
             Vector3 forceDirection = (centerOfGravity.transform.position - obj.transform.position).normalized;
             obj.GetComponent<Rigidbody>().AddForce(forceDirection * attractionForce);
--- Assets/Scripts/RepulsiveBulletBehaviour.cs
+++ Assets/Scripts/RepulsiveBulletBehaviour.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class RepulsiveBulletBehaviour : BulletBehaviour
 {
     public float repulsionForce = 9.81f;
+    public float repulsionRange = 10f; //meters
 
     protected override void ApplyForce(GameObject obj)
     {
-        if (obj != centerOfGravity)
+        if (obj != centerOfGravity && Vector3.Distance(centerOfGravity.transform.position, obj.transform.position) <= repulsionRange)
         {
             Vector3 repelDirection = (obj.transform.position - centerOfGravity.transform.position).normalized;
             obj.GetComponent<Rigidbody>().AddForce(repelDirection * repulsionForce);
